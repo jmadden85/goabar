e7cc705ac82d6d67b69a3591ddce9d1ba4ed5d38
Joe Madden <Joe@beardbro.com>
2013-05-13 02:05:21 -0400

    recoding this shit

--- js/main.js
+++ js/main.js
@@ -1,153 +1,218 @@
-var shareWrap = function () {
-    var startingPoint;
-    var GoalSlider = function (container) {
+var shareWrap = function ($) {
 
-        this.container = container;
-        this.offSetL = document.getElementsByClassName('wrapper')[0].offsetLeft;
-        this.progress = document.getElementsByClassName('progress')[0];
-        this.theBar = document.getElementsByClassName('bar')[0];
-        this.currValue = parseInt(document.getElementById('curr').value);
-        this.goalValue = parseInt(document.getElementById('goal').value);
-        this.totalValue = parseInt(document.getElementById('total').value);
-        this.barWidth = this.progress.offsetWidth;
-        this.dragger = document.getElementById('dragger');
-        this.goalTip = document.getElementById('goalNum');
-        this.totalTip = document.getElementById('totalNum');
-        this.goalMet = false;
-        this.dragHovered = false;
-        this.hover = false;
-        this.clicked = false;
+    var GoalSlider = function (min, max, goal, current, referAmount) {
+        this.min = min;
+        this.max = max;
+        this.goal = goal;
+        this.current = current;
+        this.referAmount = referAmount;
+        var moneyIndicators = $('.cashMoneyWrapper .indicators span');
+        var peopleIndicators = $('.peopleWrapper .indicators span');
+        var barWrappers = $('.barWrappers');
+        var moneyBar = $('#cashMoney .bar');
+        var peopleBar = $('#people .bar');
+        var dragger = $('.dragger');
+        var cashTooltip = $('#cashMoneyCurrent');
+        var cashGoalTooltip = $('#cashMoneyGoal');
+        var peopleTooltip = $('#peopleCurrent');
+        var peopleGoalTooltip = $('#peopleGoal');
 
-    };
+        var minNum = parseInt(this.min, 10);
+        var maxNum = parseInt(this.max, 10);
+        var goalNum = parseInt(this.goal, 10);
+        var currentNum = parseInt(this.current, 10);
+        var referAmountNum = parseInt(this.referAmount, 10);
+        var completePercentage = 100 * (currentNum / maxNum);
+        var goalPercentage = goalNum / maxNum;
+        var indicators = [minNum, maxNum / 2, maxNum];
+        var minRefer = minNum / referAmountNum;
+        var maxRefer = maxNum / referAmountNum;
+        var currentRefer = currentNum / referAmountNum;
+        var referIndicators = [minRefer, maxRefer / 2, maxRefer];
 
-    GoalSlider.prototype.init = function () {
-        var goalAdjuster = this.goalAdjuster;
-        var dragHovered = this.dragHovered;
-        if ( this.goalValue === 0 ) { //Check if goal has been set ever
-            this.goalValue = undefined;
-        } else if ( this.goalValue < this.currValue ) { // or if it has been completed already
-            this.goalMet = true;
-        }
+        this.init = function () {
 
-        //Event listener for hover events
-        this.progress.addEventListener('mouseover', function (e) {
-            goalAdjuster('mouseover', e);
-        }, false);
-        this.progress.addEventListener('mouseout', function (e) {
-            goalAdjuster('mouseout', e);
-        }, false);
+            //Set indicators
+            for ( var i = 0; i < 3; i++ ) {
+                $(moneyIndicators[i]).html('$' + indicators[i]);
+                $(peopleIndicators[i]).html(referIndicators[i]);
+            }
 
-        //Event listener for dragger hover events
-        this.dragger.addEventListener('mouseover', function (e) {
-            goalAdjuster('mouseover', e);
-        }, false);
-        this.dragger.addEventListener('mouseout', function (e) {
-            goalAdjuster('mouseout', e);
-        }, false);
+            //Load bars
+            $(moneyBar[0]).css('width', completePercentage + '%');
+            $(peopleBar[0]).css('width', completePercentage + '%');
+            $('.goalBar').css('width', (goalPercentage * 100) - completePercentage + '%');
 
-        //Event listener for mouse move events
-        this.progress.addEventListener('mousemove', function (e) {
+            //Load goal marker
+            dragger.css({
+                'left' : barWrappers.width() * goalPercentage - 17 + 'px',
+                'display' : 'block'
+            });
 
-        }, false);
-        this.dragger.addEventListener('mousemove', function (e) {
-            goalAdjuster('mousemove', e);
-        }, false);
+            //Load the tooltips
+            cashTooltip.children('.tooltip-inner').html(currentNum);
+            cashGoalTooltip.children('.tooltip-inner').html(goalNum);
+            peopleTooltip.children('.tooltip-inner').html(currentRefer);
+            peopleGoalTooltip.children('.tooltip-inner').html(goalNum / referAmountNum);
 
-        //Event listener for drag events
-        this.progress.addEventListener('drag', function (e) {
-        }, false);
+            setTimeout(function () {
+                cashTooltip.css({
+                    'left' : barWrappers.width() * (completePercentage / 100) - (cashTooltip.width() / 2) + 'px',
+                    'display' : 'block'
+                });
+                cashGoalTooltip.css({
+                    'left' : -(cashGoalTooltip.width() / 2) + 17 + 'px',
+                    'display' : 'block'
+                });
+                peopleTooltip.css({
+                    'left' : barWrappers.width() * (completePercentage / 100) - (peopleTooltip.width() / 2) + 'px',
+                    'display' : 'block'
+                });
+                peopleGoalTooltip.css({
+                    'left' : -(peopleGoalTooltip.width() / 2) + 17 + 'px',
+                    'display' : 'block'
+                });
+            }, 600);
 
-        //Event listener for click events
-        this.progress.addEventListener('mousedown', function (e) {
-            goalAdjuster('mousedown', e);
-        }, false);
-        this.progress.addEventListener('mouseup', function (e) {
-            goalAdjuster('mouseup', e);
-        }, false);
-        this.dragger.addEventListener('mouseup', function (e) {
-            goalAdjuster('mouseup', e);
-        })
+            //Set points for goal marker to snap to
+            dragger.attr('data-points', maxRefer);
+            dragger.attr('data-goal', goalNum / referAmountNum);
 
 
-        if ( !this.goalMet && this.goalValue ) {
-            this.loadBar();
-        }
-    };
+        };
 
-    GoalSlider.prototype.loadBar = function () {
-        //get percentage
-        this.goalPercentage = this.currValue / this.goalValue * 100;
-        //get and set goal value
-        var goalTipValue = this.goalTip.children[1];
-        goalTipValue.innerHTML = this.goalValue;
-        //Move goal tooltip to end of bar
-        var widthToSubtract = this.goalTip.offsetWidth / 2;
+        this.slideChange = function (sliding, num) {
+            this.sliding = sliding;
+            this.num = num;
+            var isAdjusting = this.sliding;
+            var theNum = parseInt(this.num, 10);
+            var thisGoal = dragger.attr('data-goal');
 
-        //animate bar percentage
-        this.goalTip.style.left = (this.barWidth - widthToSubtract) + 'px';
-        this.theBar.style.width = this.goalPercentage + '%';
-    };
+            if ( !isAdjusting ) {
+                dragger.css({
+                    'left' : barWrappers.width() * (thisGoal / maxRefer) - 17 + 'px',
+                    'display' : 'block'
+                });
+                return false;
+            }
 
-    GoalSlider.prototype.goalAdjuster = function (e, eObj) {
-        var offSetL = document.getElementsByClassName('wrapper')[0].offsetLeft;
-        var barWidth = document.getElementsByClassName('progress')[0].offsetWidth;
-        var onePercenter = barWidth / 100;
-        var currentPoint;
-        var currentPercent;
-        if ( e === 'mousedown' ) { //on mouse down on bar
-            this.clicked = true;
-            console.log(eObj);
-            this.dragger.style.left = (eObj.offsetX - 25) + 'px';
-            this.dragger.style.display = 'block';
-            startingPoint = eObj.offsetX - 25;
-        } else if ( e === 'mouseup' ) { //on mouse up on bar
-            this.clicked = false;
-            this.dragger.style.display = 'none';
-        } else if ( e === 'mouseover' ) { //on mouse over
-            this.hovered = true;
-            // setTimeout(function () {
-            //     this.hovered ? this.dragger.style.display = 'block' : this.dragger;
-            // }, 500);
-        } else if ( e === 'mouseout' ) { //on mouse out
-            this.hovered = false;
-            // setTimeout(function () {
-            //     !this.hovered && !this.dragHovered ? this.dragger.style.display = 'none' : this.dragger;
-            // }, 500);
-        } else if ( e === 'mousemove' ) { //on move
-            var pos = (eObj.pageX - offSetL) - 25;
-            var theBar = document.getElementsByClassName('bar')[0];
-            var currentIncrement =
-            this.dragger.style.left = pos + 'px';
-            currentPoint = pos - startingPoint;
-            currentPercent = Math.ceil(currentPoint / Math.round(onePercenter));
-        }
+            dragger.attr('data-goal', theNum);
+            cashGoalTooltip.children('.tooltip-inner').html(theNum * referAmountNum);
+            peopleGoalTooltip.children('.tooltip-inner').html(theNum);
+
+            peopleGoalTooltip.css({
+                'left' : -(peopleGoalTooltip.width() / 2) + 17 + 'px',
+                'display' : 'block'
+            });
+
+            cashGoalTooltip.css({
+                'left' : -(cashGoalTooltip.width() / 2) + 17 + 'px',
+                'display' : 'block'
+            });
+
+        };
+
     };
 
     document.addEventListener('DOMContentLoaded', function() {
-        var goalSet = new GoalSlider('goalBarWrapper');
+        var current = $('#current').val();
+        var goal = $('#goal').val();
+        var min = $('#min').val();
+        var max = $('#max').val();
+        var referAmount = $('#referAmount').val();
+        var goalSet = new GoalSlider(min, max, goal, current, referAmount);
+        var dragger = $('.dragger');
+        var dragging = false;
+        var bars = $('.barWrappers');
+        var minBar = $('#minBar').val();
+        var maxBar = $('#maxBar').val();
+        var setter = $('a.btn');
+        var barWidth = bars.width();
+        var goalMidPoints = [];
+
+        setter.click(function () {
+
+        });
+
         goalSet.init();
+
+        var dragPoints = parseInt(dragger.attr('data-points'), 10);
+        var spaceBetweenGoals = barWidth / dragPoints;
+
+        for ( var i = 0; i < (dragPoints - 1); i++ ) {
+            var startingSpace = spaceBetweenGoals * 1.5;
+            var thisSpace = spaceBetweenGoals * i;
+            goalMidPoints.push(Math.round(startingSpace + thisSpace));
+        }
+
+        console.log(goalMidPoints);
+
+        dragger.on('mousedown mouseup mousemove', function (event) {
+            var eventType = event.type;
+            var mouseX = event.offsetX;
+            var mouseY = event.offsetY;
+            event.preventDefault();
+
+            switch (eventType) {
+
+            case 'mousedown' :
+                dragging = true;
+                break;
+
+            case 'mouseup' :
+                dragging = false;
+                break;
+
+            default :
+                break;
+
+            }
+
+
+        });
+
+        $(document).on('mouseup', function () {
+            dragging = false;
+        });
+
+        bars.on('mouseup mousemove', function (event) {
+            var eventType = event.type;
+            event.preventDefault();
+
+            switch (eventType) {
+
+            case 'mouseup' :
+                dragging = false;
+                goalSet.slideChange(false);
+                break;
+
+            case 'mousemove' :
+                var mouseX = (event.pageX - bars.offset().left) - 17;
+                var mouseY = event.offsetY;
+                var goalNow = dragger.attr('data-goal');
+                if ( dragging && mouseX > -17 && mouseX < (barWidth - 17) ) {
+
+                    //Increment while changing
+                    if ( mouseX < goalMidPoints[goalNow - 2] ) {
+                        goalSet.slideChange(true, goalNow - 1);
+                    } else if ( mouseX > goalMidPoints[goalNow - 1] ) {
+                        goalSet.slideChange(true, parseInt(goalNow, 10) + 1);
+                    }
+
+                    dragger.css('left', mouseX + 'px');
+                    console.log(parseInt(dragger.css('left').split('px')[0], 10) + ', ' + $($('.bar')[0]).width() + ', ' + barWidth);
+                    $('.goalBar').css('width', (parseInt(dragger.css('left').split('px')[0], 10) / barWidth * 100) - $($('.bar')[0]).width() / barWidth * 100 + '%');
+                }
+                break;
+
+            default :
+                break;
+
+            }
+
+
+        });
+
     }, false);
 
-// var shareIIN = function () {
-//     var GoalBar = function (id) {
-//         this.id = id;
-//         this.container = document.getElementById(this.id);
-//         this.bar = document.querySelector('#' + this.id + ' .progress .bar');
-//         this.progress = document.querySelector('#' + this.id + ' .progress');
-//         this.goal = document.querySelector('#' + this.id + ' #goalNum');
-//         this.total = document.querySelector('#' + this.id + ' #totalNum');
-//         this.totalNum = parseInt(document.querySelector('#' + this.id + ' #total').value);
-//         this.goalNum = parseInt(document.querySelector('#' + this.id + ' #goal').value);
-//         this.currNum = parseInt(document.querySelector('#' + this.id + ' #current').value);
-//         this.goalPercentage = (this.goalNum / this.totalNum) * 100;
-//         this.currPercentage = (this.currNum / this.totalNum) * 100;
-//         this.barWidth = this.progress.style.width;
-//     };
-
-//     GoalBar.prototype.initBar = function () {
-//         this.bar.style.width = this.currPercentage + '%';
-//     };
-
-//     var referralsGoal = new GoalBar ('goalSetter');
-//     referralsGoal.initBar();
-}();
+}(jQuery);
---
+++ js/oldMain.js
@@ -0,0 +1,152 @@
+var shareWrap = function () {
+    var startingPoint;
+    var GoalSlider = function (container) {
+
+        this.container = container;
+        this.offSetL = document.getElementsByClassName('wrapper')[0].offsetLeft;
+        this.progress = document.getElementsByClassName('progress')[0];
+        this.theBar = document.getElementsByClassName('bar')[0];
+        this.currValue = parseInt(document.getElementById('curr').value);
+        this.goalValue = parseInt(document.getElementById('goal').value);
+        this.totalValue = parseInt(document.getElementById('total').value);
+        this.barWidth = this.progress.offsetWidth;
+        this.dragger = document.getElementById('dragger');
+        this.goalTip = document.getElementById('goalNum');
+        this.totalTip = document.getElementById('totalNum');
+        this.goalMet = false;
+        this.dragHovered = false;
+        this.hover = false;
+        this.clicked = false;
+    };
+
+    GoalSlider.prototype.init = function () {
+        var goalAdjuster = this.goalAdjuster;
+        var dragHovered = this.dragHovered;
+        if ( this.goalValue === 0 ) { //Check if goal has been set ever
+            this.goalValue = undefined;
+        } else if ( this.goalValue < this.currValue ) { // or if it has been completed already
+            this.goalMet = true;
+        }
+
+        //Event listener for hover events
+        this.progress.addEventListener('mouseover', function (e) {
+            goalAdjuster('mouseover', e);
+        }, false);
+        this.progress.addEventListener('mouseout', function (e) {
+            goalAdjuster('mouseout', e);
+        }, false);
+
+        //Event listener for dragger hover events
+        this.dragger.addEventListener('mouseover', function (e) {
+            goalAdjuster('mouseover', e);
+        }, false);
+        this.dragger.addEventListener('mouseout', function (e) {
+            goalAdjuster('mouseout', e);
+        }, false);
+
+        //Event listener for mouse move events
+        this.progress.addEventListener('mousemove', function (e) {
+
+        }, false);
+        this.dragger.addEventListener('mousemove', function (e) {
+            goalAdjuster('mousemove', e);
+        }, false);
+
+        //Event listener for drag events
+        this.progress.addEventListener('drag', function (e) {
+        }, false);
+
+        //Event listener for click events
+        this.progress.addEventListener('mousedown', function (e) {
+            goalAdjuster('mousedown', e);
+        }, false);
+        this.progress.addEventListener('mouseup', function (e) {
+            goalAdjuster('mouseup', e);
+        }, false);
+        this.dragger.addEventListener('mouseup', function (e) {
+            goalAdjuster('mouseup', e);
+        })
+
+
+        if ( !this.goalMet && this.goalValue ) {
+            this.loadBar();
+        }
+    };
+
+    GoalSlider.prototype.loadBar = function () {
+        //get percentage
+        this.goalPercentage = this.currValue / this.goalValue * 100;
+        //get and set goal value
+        var goalTipValue = this.goalTip.children[1];
+        goalTipValue.innerHTML = this.goalValue;
+        //Move goal tooltip to end of bar
+        var widthToSubtract = this.goalTip.offsetWidth / 2;
+
+        //animate bar percentage
+        this.goalTip.style.left = (this.barWidth - widthToSubtract) + 'px';
+        this.theBar.style.width = this.goalPercentage + '%';
+    };
+
+    GoalSlider.prototype.goalAdjuster = function (e, eObj) {
+        var offSetL = document.getElementsByClassName('wrapper')[0].offsetLeft;
+        var barWidth = document.getElementsByClassName('progress')[0].offsetWidth;
+        var onePercenter = barWidth / 100;
+        var currentPoint;
+        var currentPercent;
+        if ( e === 'mousedown' ) { //on mouse down on bar
+            this.clicked = true;
+            console.log(eObj);
+            this.dragger.style.left = (eObj.offsetX - 25) + 'px';
+            this.dragger.style.display = 'block';
+            startingPoint = eObj.offsetX - 25;
+        } else if ( e === 'mouseup' ) { //on mouse up on bar
+            this.clicked = false;
+            this.dragger.style.display = 'none';
+        } else if ( e === 'mouseover' ) { //on mouse over
+            this.hovered = true;
+            // setTimeout(function () {
+            //     this.hovered ? this.dragger.style.display = 'block' : this.dragger;
+            // }, 500);
+        } else if ( e === 'mouseout' ) { //on mouse out
+            this.hovered = false;
+            // setTimeout(function () {
+            //     !this.hovered && !this.dragHovered ? this.dragger.style.display = 'none' : this.dragger;
+            // }, 500);
+        } else if ( e === 'mousemove' ) { //on move
+            var pos = (eObj.pageX - offSetL) - 25;
+            var theBar = document.getElementsByClassName('bar')[0];
+            var currentIncrement =
+            this.dragger.style.left = pos + 'px';
+            currentPoint = pos - startingPoint;
+            currentPercent = Math.ceil(currentPoint / Math.round(onePercenter));
+        }
+    };
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var goalSet = new GoalSlider('goalBarWrapper');
+        goalSet.init();
+    }, false);
+
+// var shareIIN = function () {
+//     var GoalBar = function (id) {
+//         this.id = id;
+//         this.container = document.getElementById(this.id);
+//         this.bar = document.querySelector('#' + this.id + ' .progress .bar');
+//         this.progress = document.querySelector('#' + this.id + ' .progress');
+//         this.goal = document.querySelector('#' + this.id + ' #goalNum');
+//         this.total = document.querySelector('#' + this.id + ' #totalNum');
+//         this.totalNum = parseInt(document.querySelector('#' + this.id + ' #total').value);
+//         this.goalNum = parseInt(document.querySelector('#' + this.id + ' #goal').value);
+//         this.currNum = parseInt(document.querySelector('#' + this.id + ' #current').value);
+//         this.goalPercentage = (this.goalNum / this.totalNum) * 100;
+//         this.currPercentage = (this.currNum / this.totalNum) * 100;
+//         this.barWidth = this.progress.style.width;
+//     };
+
+//     GoalBar.prototype.initBar = function () {
+//         this.bar.style.width = this.currPercentage + '%';
+//     };
+
+//     var referralsGoal = new GoalBar ('goalSetter');
+//     referralsGoal.initBar();
+}();
